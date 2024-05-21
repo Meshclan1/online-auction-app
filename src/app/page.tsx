3234afe131fc81e0ea8f54401bc316bd833d2c8e
Ayo <Ayo.meshe@gmail.com>
2024-05-21 13:57:42 +0100

    revalidate after adding items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { database } from "@/db/database";
 import { bids as bidSchema } from "@/db/schema";
+import { revalidatePath } from "next/cache";
 
 export default async function Home() {
   const bids = await database.query.bids.findMany();
@@ -10,10 +13,12 @@ export default async function Home() {
         action={async (formData: FormData) => {
           "use server";
           await database.insert(bidSchema).values({});
+          revalidatePath("/");
+          // this tells nextjs to reload our component & associated dataß
         }}
       >
-        <input name="bid" placeholder="Bid" />
-        <button type="submit">Place Bid</button>
+        <Input name="bid" placeholder="Bid" />
+        <Button type="submit">Place Bid</Button>
       </form>
       {bids.map((bid) => (
         <div key={bid.id}>{bid.id}</div>
